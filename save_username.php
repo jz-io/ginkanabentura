acee6989c2059c6121147b3d7c23cc331ba125ab
jzk <115957048+jz-io@users.noreply.github.com>
2025-02-22 13:01:16 +0100

    Update save_username.php

--- save_username.php
+++ save_username.php
@@ -2,7 +2,7 @@
 $data = json_decode(file_get_contents('php://input'), true);
 $username = $data['username'];
 $answer = $data['answer'];
-$filename = "HTML/Emaitzak/emaitza_{$username}.txt";
+$filename = "Emaitzak/emaitza_{$username}.txt";
 
 file_put_contents($filename, "Answer: {$answer}\n", FILE_APPEND);
 echo "Answer saved successfully.";
